17326b280c691ab5617c225a6bd350de3fc4180d
Trish Ta <trish.ta@shopify.com>
2021-08-17 10:35:41 -0400

    Create JS build script and added test extension

---
+++ cli/testdata/checkout-ui-extension/src/index.tsx
@@ -0,0 +1,28 @@
+import {
+  render,
+  TextField,
+  TextBlock,
+  BlockStack,
+} from '@shopify/checkout-ui-extensions-react';
+
+render('Checkout::Feature::Render', ({extensionPoint}) => (
+  <App extensionPoint={extensionPoint} />
+));
+
+function App({extensionPoint}: {extensionPoint: string}) {
+  return (
+    <BlockStack>
+      <TextBlock>Welcome to the {extensionPoint} extension!</TextBlock>
+      <TextBlock>My custom environment variable is: {process.env.SOME_VAR}</TextBlock>
+      <TextBlock>My custom NODE_ENV is: {process.env.NODE_ENV}</TextBlock>
+      <TextBlock>{window.test}</TextBlock>
+      <TextField
+        label="Order note"
+        onChange={(value) => {
+          // eslint-disable-next-line no-console
+          console.log(`Updated order note: ${value}`);
+        }}
+      />
+    </BlockStack>
+  );
+}
