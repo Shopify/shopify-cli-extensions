f24a9e1f31e86ec0bbb0e2e081f3a57999ffa128
Elana Kopelevich <elana.kopelevich@shopify.com>
2021-10-21 10:03:12 -0600

    feat/storybook a11y addon

--- packages/dev-console/.storybook/main.js
+++ packages/dev-console/.storybook/main.js
@@ -1,19 +1,17 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
     {
       name: '@storybook/preset-scss',
       options: {
         cssLoaderOptions: {
-           modules: true,
+          modules: true,
           //  localIdentName: '[name]__[local]--[hash:base64:5]',
-        }
-      }
+        },
+      },
     },
-  ]
-}
+  ],
+};
