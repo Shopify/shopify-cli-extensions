f7b37dc31ea9f87366e8a73152a52c10222e3407
Ricky Mao <ricky.mao@shopify.com>
2021-11-04 15:01:29 -0400

    chore: removed the i18n commented code

--- packages/ui-extensions-server-kit/src/components/ActionSet/actions/RefreshAction/RefreshAction.tsx
+++ packages/ui-extensions-server-kit/src/components/ActionSet/actions/RefreshAction/RefreshAction.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-// import {useI18n} from '@shopify/react-i18n';
 import {useDevConsole} from '../../../../state/context';
 import {RefreshMinor} from '../../../Icon/icons';
 import {Action} from '../../Action';
-import {Action as ActionType} from '../../../../types';
 import {useExtensions} from '../../../../utilities/extensionPayload';
 
 export function RefreshAction() {
-  // const [i18n] = useI18n();
   const {dispatch} = useDevConsole();
   const extensions = useExtensions();
 
--- packages/ui-extensions-server-kit/src/components/ActionSet/actions/ToggleViewAction/ToggleViewAction.tsx
+++ packages/ui-extensions-server-kit/src/components/ActionSet/actions/ToggleViewAction/ToggleViewAction.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
 import {HideMinor, ViewMinor} from '../../../Icon/icons';
-// import {useI18n} from '@shopify/react-i18n';
 import {useDevConsole} from '../../../../state/context';
 import {useExtensions} from '../../../../utilities/extensionPayload';
 import {Action} from '../../Action';
 
 export function ToggleViewAction() {
   const extensions = useExtensions();
-  // const [i18n] = useI18n();
 
   const atLeastOneExtensionVisible = extensions.some(
     (extensionManifest) => !extensionManifest.development.hidden,
