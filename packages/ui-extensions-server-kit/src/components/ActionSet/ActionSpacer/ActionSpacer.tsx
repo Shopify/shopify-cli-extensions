ecbdfe29b0414867df72351bb645a4f516f4cf97
Ricky Mao <ricky.mao@shopify.com>
2021-11-08 09:41:03 -0800

    chore: added top-level documentation to ActionSpacer

--- packages/ui-extensions-server-kit/src/components/ActionSet/ActionSpacer/ActionSpacer.tsx
+++ packages/ui-extensions-server-kit/src/components/ActionSet/ActionSpacer/ActionSpacer.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 import styles from './ActionSpacer.scss';
 
+/**
+ * Adds spacing between actions in an action set. Useful for matching ExtensionHeader and ExtensionRow actions alignment.
+ */
 export function ActionSpacer() {
   return <div className={styles.Spacer} />;
 }
