5a8b1051d941f0570f93986b188a2c014348f903
Ricky Mao <ricky.mao@shopify.com>
2021-11-08 14:05:44 -0800

    fix: testing webpack config

--- packages/ui-extensions-server-kit/.storybook/main.js
+++ packages/ui-extensions-server-kit/.storybook/main.js
@@ -15,4 +15,106 @@ module.exports = {
       },
     },
   ],
+  webpackFinal: async (config) => {
+    config.module.rules.push(      {
+      test: /\.scss$/,
+      use: [
+        {
+          loader: 'style-loader',
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            esModule: true,
+            importLoaders: 1,
+            modules: {
+              auto: (resourcePath) =>
+                !resourcePath.includes('node_modules/') ||
+                resourcePath.endsWith('.scss'),
+              localIdentName: '[name]-[local]_[hash:base64:5]',
+            },
+          },
+        },
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [['@shopify/postcss-plugin', {minimize: false}]],
+            },
+          },
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            implementation: require('node-sass'),
+            sassOptions: {
+              includePaths: [path.resolve(__dirname, '../app/styles')],
+            },
+          },
+        },
+        {
+          loader: 'sass-resources-loader',
+          options: {
+            resources: [
+              path.resolve(
+                __dirname,
+                '../node_modules/@shopify/polaris/dist/styles/_public-api.scss',
+              ),
+            ],
+          },
+        },
+      ],
+    },
+    {
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+      resolve: {
+        fullySpecified: false,
+      },
+    },
+    {
+      test: /icons\/.*\.svg$/,
+      use: [
+        {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [
+              ['@shopify/babel-preset', {typescript: true, react: true}],
+            ],
+          },
+        },
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: svgOptimizationOptions(),
+            replaceAttrValues: {
+              // Old design language
+              '#FFF': 'currentColor',
+              '#fff': 'currentColor',
+              '#637381': '{undefined}',
+              '#212B36': '{undefined}',
+              '#212b36': '{undefined}',
+
+              // New design language
+              '#5C5F62': '{undefined}',
+              '#5c5f62': '{undefined}',
+            },
+            babel: false,
+          },
+        },
+      ],
+    },
+    {
+      test: /\.graphql$/,
+      use: [
+        {
+          loader: 'graphql-mini-transforms/webpack-loader',
+          options: {simple: true},
+        },
+      ],
+    },);
+
+    return config;
+  }
 };
