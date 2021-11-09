824f9637f59a64237dce55bd9b36d5bb33247b7c
Ricky Mao <ricky.mao@shopify.com>
2021-11-09 08:59:38 -0800

    feat: added description for Status and removed text spaces

--- packages/ui-extensions-server-kit/src/components/Status/Status.tsx
+++ packages/ui-extensions-server-kit/src/components/Status/Status.tsx
@@ -15,6 +15,7 @@ export interface StatusProps {
   status?: StatusType;
 }
 
+/** Status shows you the connection state of an extension */
 export function Status({status = StatusType.Connected}) {
   const statusClass = classNames(styles.Status, styles[status] ?? styles.BuildError);
   const text = status;
