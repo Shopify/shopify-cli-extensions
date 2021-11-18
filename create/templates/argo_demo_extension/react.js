6d7c9f6a1082b1fd90eb9084294a9ae71c4369aa
Trish Ta <trish.ta@shopify.com>
2021-11-18 15:52:35 -0500

    Fix react remplates for Admin extensions

--- create/templates/argo_demo_extension/react.js
+++ create/templates/argo_demo_extension/react.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import {render, extend, Text} from '@shopify/admin-ui-extensions-react';
 
-extend('Playground', render(App));
+extend(
+  'Playground',
+  render(() => <App />),
+);
 
 function App({extensionPoint}) {
   return <Text>Welcome to the {extensionPoint} extension!</Text>;
