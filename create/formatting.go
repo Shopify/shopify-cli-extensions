fc95b94f035ae0aeafe09c5df4f69997b46fb373
Konstantin Tennhard <konstantin.tennhard@shopify.com>
2022-03-31 14:47:48 -0400

    Remove dead code

--- create/formatting.go
+++ create/formatting.go
@@ -3,27 +3,14 @@ package create
 import (
 	"bytes"
 	"encoding/json"
-	"strings"
 
-	"github.com/Shopify/shopify-cli-extensions/core"
-	"github.com/Shopify/shopify-cli-extensions/core/fsutils"
 	"gopkg.in/yaml.v3"
 )
 
-func formatContent(targetPath string, content []byte) ([]byte, error) {
-	if strings.HasSuffix(targetPath, fsutils.JSON) {
-		return formatJSON(content)
-	}
-
-	if strings.HasSuffix(targetPath, fsutils.YAML) {
-		return formatYaml(content, targetPath)
-	}
-	return content, nil
-}
-
 func formatJSON(input []byte) ([]byte, error) {
-	var result map[string]interface{}
-	if err := json.Unmarshal(input, &result); err != nil {
+	var data map[string]interface{}
+
+	if err := json.Unmarshal(input, &data); err != nil {
 		return nil, err
 	}
 
@@ -31,42 +18,25 @@ func formatJSON(input []byte) ([]byte, error) {
 	encoder := json.NewEncoder(&output)
 	encoder.SetEscapeHTML(false)
 	encoder.SetIndent("", "  ")
-	if err := encoder.Encode(result); err != nil {
+	if err := encoder.Encode(data); err != nil {
 		return nil, err
 	}
 
 	return output.Bytes(), nil
 }
 
-func formatYaml(unformattedContent []byte, targetPath string) (content []byte, err error) {
-	var b bytes.Buffer
-	var config interface{}
-
-	if strings.HasSuffix(targetPath, cliConfigYamlFile) {
-		config = shopifyCLIYML{}
-	} else if strings.HasSuffix(targetPath, configYamlFile) {
-		config = core.Extension{}
-	} else {
-		return
+func formatYaml(input []byte, targetPath string) ([]byte, error) {
+	var data map[interface{}]interface{}
+	if err := yaml.Unmarshal(input, &data); err != nil {
+		return nil, err
 	}
 
-	err = yaml.Unmarshal(unformattedContent, &config)
+	var output bytes.Buffer
+	output.Write([]byte("---\n"))
 
-	if err != nil {
-		return
-	}
+	encoder := yaml.NewEncoder(&output)
+	encoder.SetIndent(2)
+	encoder.Encode(&data)
 
-	yamlEncoder := yaml.NewEncoder(&b)
-	yamlEncoder.SetIndent(2)
-	yamlEncoder.Encode(&config)
-
-	content = []byte("---\n")
-	content = append(content, b.Bytes()...)
-	return
-}
-
-type shopifyCLIYML struct {
-	ProjectType    string `yaml:"project_type"`
-	OrganizationId string `yaml:"organization_id"`
-	ExtensionType  string `yaml:"EXTENSION_TYPE"`
+	return output.Bytes(), nil
 }
