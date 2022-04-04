cab92433d0cdf7aec2b7d930b0a734584714c167
Konstantin Tennhard <konstantin.tennhard@shopify.com>
2022-04-04 11:23:23 -0400

    Remove merge functionality for now
    This feature will be shipped as a separate PR algonside with template
    deduplication.

--- create/templating.go
+++ create/templating.go
@@ -1,17 +1,13 @@
 package create
 
 import (
-	"bytes"
 	"fmt"
 	"html/template"
 	"io"
 	"io/fs"
-	"reflect"
 	"strings"
 
 	"github.com/Shopify/shopify-cli-extensions/core"
-	"github.com/imdario/mergo"
-	"gopkg.in/yaml.v3"
 )
 
 func NewTemplateEngine(extension core.Extension, shared, project FS) *templateEngine {
@@ -96,76 +92,6 @@ func buildTemplateHelpers(t *template.Template, extension core.Extension, shared
 				panic(err)
 			}
 			return template.HTML(string(data))
-		},
-		"merge": func(paths ...string) string {
-			fragments := make([]core.Fragment, 0, len(paths))
-
-			for _, path := range paths {
-				buffer := bytes.Buffer{}
-				t.ExecuteTemplate(&buffer, path, extension)
-
-				fragment := make(core.Fragment)
-				yaml.Unmarshal(buffer.Bytes(), fragment)
-				fragments = append(fragments, fragment)
-			}
-
-			mergeFn := func(fragments ...core.Fragment) core.Fragment {
-				result := fragments[0]
-				for _, fragment := range fragments[1:] {
-					err := mergo.Merge(&result, &fragment, mergo.WithAppendSlice)
-					if err != nil {
-						fmt.Println(err)
-					}
-				}
-				return result
-			}
-			result := mergeFn(fragments...)
-
-			deduplicateFn := func(merged core.Fragment) core.Fragment {
-				deduped := make(core.Fragment)
-				for key, value := range merged {
-					if reflect.TypeOf(value) != reflect.TypeOf([]interface{}{}) {
-						deduped[key] = value
-						continue
-					}
-					// assert: value is a slice
-
-					src := value.([]interface{})
-					dst := make([]interface{}, 0, len(src))
-
-					if reflect.TypeOf(src[0]) == reflect.TypeOf(map[string]interface{}{}) {
-						/*
-						 * Assertion - value is a slice of maps
-						 * O(n^2) approach used with inner maps because Go maps
-						 * are not comparable and thus cannot act as key values
-						 */
-					outer:
-						for _, srcMap := range src {
-							for _, dstMap := range dst {
-								if reflect.DeepEqual(srcMap, dstMap) {
-									continue outer
-								}
-							}
-							dst = append(dst, srcMap)
-						}
-					} else {
-						// Assertion - value is a slice (but not a slice of maps)
-						set := map[interface{}]struct{}{}
-						for _, l := range src {
-							if _, exists := set[l]; !exists {
-								set[l] = struct{}{}
-								dst = append(dst, l)
-							}
-						}
-					}
-					deduped[key] = dst
-				}
-				return deduped
-			}
-			result = deduplicateFn(result)
-
-			serializedResult, _ := yaml.Marshal(result)
-			return strings.TrimSpace(string(serializedResult))
 		},
 	}
 }
