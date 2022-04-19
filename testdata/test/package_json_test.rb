89bb38ab830a3a5f5244100c2a9d4b603f7adaea
David Henry <david.henry@shopify.com>
2022-04-19 11:07:54 -0400

    Make Rubocop changes.

--- testdata/test/package_json_test.rb
+++ testdata/test/package_json_test.rb
@@ -13,7 +13,7 @@ module IntegrationTest
       package_json_contents = File.read('tmp/integration_test/package.json')
       package_json = JSON.parse(package_json_contents)
       dependencies = package_json['dependencies']
-      devDependencies = package_json['devDependencies']
+      dev_dependencies = package_json['devDependencies']
       scripts = package_json['scripts']
 
       assert_equal('integration_test', package_json['name'])
@@ -23,8 +23,8 @@ module IntegrationTest
       assert_equal('^15.5.1', dependencies['graphql'])
       assert_equal('^2.12.4', dependencies['graphql-tag'])
       assert_equal('^3.4.8', dependencies['@apollo/client'])
-      assert_equal('^4.1.0', devDependencies['typescript'])
-      assert_equal('latest', devDependencies['@shopify/shopify-cli-extensions'])
+      assert_equal('^4.1.0', dev_dependencies['typescript'])
+      assert_equal('latest', dev_dependencies['@shopify/shopify-cli-extensions'])
       assert_equal('shopify-cli-extensions build', scripts['build'])
       assert_equal('shopify-cli-extensions develop', scripts['develop'])
     end
